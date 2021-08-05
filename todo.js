05e1b43bb4c52cbd82b1da59900dd0126f4f2912
fhsi1 <eugenie8@kakao.com>
2021-08-05 15:54:21 +0900

    [UPDATE] relocation cross

--- todo.js
+++ todo.js
@@ -27,10 +27,11 @@ function paintToDo(text) {
 	const span = document.createElement("span");
 	const newId = toDos.length + 1;
 	delBtn.innerText = "❌";
+	delBtn.className = "todo_btn";
 	delBtn.addEventListener("click", deleteToDo);
 	span.innerText = text;
-	li.appendChild(delBtn);
 	li.appendChild(span);
+	li.appendChild(delBtn);
 	li.id = newId;
 	toDoList.appendChild(li);
 	const toDoObj = {
